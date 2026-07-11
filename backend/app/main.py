735556c0b0afd343083ed04f10924609c82fadfe
subrajith05 <subrajith.r@gmail.com>
2026-07-12 02:46:46 +0530

    Fixed CORS error

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,10 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://documind-backend-n4me.onrender.com",
-    ],
+    allow_origins=settings.ALLOWED_ORIGINS.split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
